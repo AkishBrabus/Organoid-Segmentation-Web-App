a2e41c36a652d29aff1d7ad5187617f9036f7093
Akash <akashsuresh1.22@gmail.com>
2024-05-02 17:33:30 -0400

    Training update

--- test.py
+++ test.py
@@ -1,20 +1,26 @@
 from EmbedSeg.utils.create_dicts import create_test_configs_dict
 from EmbedSeg.test import begin_evaluating
 
-configs = create_test_configs_dict(data_dir = r"C:/Users/cryst/Desktop/Test Images",
-                                        checkpoint_path = r"C:/Users/cryst/Desktop/Organoid Segmentation Web App/models/best_iou_model.pth",
-                                        tta = True,
-                                        ap_val = 0.5,
-                                        min_object_size = 2,
-                                        save_dir = r"C:/Users/cryst/Desktop/Organoid Segmentation Web App/temp",
-                                        norm = 'min=max-percentile',
-                                        data_type = '8-bit',
-                                        one_hot = False,
-                                        n_y = 1038,
-                                        n_x = 1388,
-                                        cluster_fast = True,
-                                        device = "cpu")
+def main():
+    configs = create_test_configs_dict(
+        data_dir = r"C:/Users/cryst/Desktop/Test Images",
+        checkpoint_path = r"C:/Users/cryst/Desktop/Organoid-Segmentation-Web-App/models/best_iou_model.pth",
+        tta = True,
+        ap_val = 0.5,
+        min_object_size = 2,
+        save_dir = r"C:/Users/cryst/Desktop/Organoid-Segmentation-Web-App/temp",
+        norm = 'min-max-percentile',
+        data_type = '8-bit',
+        one_hot = False,
+        n_y = 1392,
+        n_x = 1392,
+        cluster_fast = True,
+        device = "cpu"
+    )
 
 
 
-result = begin_evaluating(configs)
+    result = begin_evaluating(configs)
+
+if __name__ == '__main__':
+    main()
